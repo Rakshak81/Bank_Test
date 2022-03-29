fbaca5f5040526e95417b182157537f2e538fcc6
Rakshak Shetty <rakshak_81@hotmail.com>
2022-03-29 09:12:05 +0100

    balance 0

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,5 +1,11 @@
 class Bank
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
   def deposit
 
   end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,3 +5,10 @@ describe Bank do
     expect(subject).to respond_to :deposit
   end
 end
+
+describe 'balance' do
+  it 'Balance starts at 0' do
+    bank = Bank.new
+    expect(bank.balance).to eq 0
+  end
+end
